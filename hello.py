f1900d9fda8f5b686f44af34cc197e6f3b7f3386
Timothy <timothymang09@gmail.com>
2026-09-04 09:25:29 -0500

    first file

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello friend!")
+print("hell ma, \"Jane\"") #\' or \" is used to include " or ' in the code.
+print("path: c:\\user\\timothy") #\\ is used to include a \ in the code.
+print("hello there,\n\t I hope you are doing good?")
+
+
+#python challenge 
+
+print("""Your Learning Path:
+      \t - Python Basics
+      \t - Data engineering
+      \t - AI""")
