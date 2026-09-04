ab00720b814d90987d5ab1bd7fad62eb9cfb16a8
Timothy <timothymang09@gmail.com>
2026-09-04 13:23:54 -0500

    len function and conditions

--- data_type.py
+++ data_type.py
@@ -1,4 +1,4 @@
-a = 10 #integer
+'''a = 10 #integer
 b = 3.5 # float
 c = "hello" #string
 d = "123" # string
@@ -9,4 +9,19 @@ f = None # this is none waiting for input.
 function.name( ) #syntax
 name.upper() #method
 # most method will work for specific data types.
- 
+'''
+
+name = "Timothy"
+age = 36
+
+print(type(name))
+print(f'your name is {name} and you are {age} yrs old')
+
+password = input("what is your password? ")
+length_of_password = len(password)
+#print(length_of_password)
+
+if length_of_password < 8:
+    print(f"password is {length_of_password} which is short")
+else:
+    print(f"the length of your password is {length_of_password} you have been given access!")
