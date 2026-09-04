a65364399c732e09b23bf8d315098a2b5bb0572f
Timothy <timothymang09@gmail.com>
2026-09-04 13:34:12 -0500

    count method

--- data_type.py
+++ data_type.py
@@ -11,7 +11,7 @@ name.upper() #method
 # most method will work for specific data types.
 '''
 
-name = "Timothy"
+'''name = "Timothy"
 age = 36
 
 print(type(name))
@@ -24,4 +24,13 @@ length_of_password = len(password)
 if length_of_password < 8:
     print(f"password is {length_of_password} which is short")
 else:
-    print(f"the length of your password is {length_of_password} you have been given access!")
+    print(f"the length of your password is {length_of_password} you have been given access!")'''
+text = ''' this is the last time i am learning python. 
+i have watched python videos on youtube, bought udemy 
+cousres of python, but did not complete them. his python 
+class by Baraa is different and i will finish the 13hrs video.'''
+
+#count method.
+
+python_numbers = text.count("python")
+print(python_numbers)
