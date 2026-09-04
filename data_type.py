143c0c04274cdd53797adaab82a2e8362d65c460
Timothy <timothymang09@gmail.com>
2026-09-04 12:47:40 -0500

    data types

---
+++ data_type.py
@@ -0,0 +1,12 @@
+a = 10 #integer
+b = 3.5 # float
+c = "hello" #string
+d = "123" # string
+e = True False #boolean
+f = None # this is none waiting for input.
+
+#function and Methods
+function.name( ) #syntax
+name.upper() #method
+# most method will work for specific data types.
+ 
