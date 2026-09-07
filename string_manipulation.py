ec059cf6c8135d2bf824ba2325cb157eacd6224c
Timothy <timothymang09@gmail.com>
2026-09-07 03:08:11 -0500

    slicing

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,4 +1,4 @@
-''' we are learning the replace, split, cat methods.'''
+''' we are learning the replace, split, cat methods.
 
 price = "34,56"
 
@@ -11,6 +11,8 @@ price = "34,56"
 phone = "+49 (176) 123-4567" #replace this number tp show 00491761234567
 
 answer = print(phone.replace("+", "00").replace("(", "").replace(")", "").replace("-", "").replace(" ", ""))
+
+
 #cat
 
 name = "kalu"
@@ -20,3 +22,17 @@ full_name = name + first_name + surname
 
 print(full_name)
 print(surname + " " + first_name)
+
+
+#split method results in a list.
+
+csv_file = "1234,Maz,USA,1970-10-30,M"
+new_file = csv_file.split(",")
+print(new_file)
+'''
+
+#slicing strings []
+
+name = "Timothy"
+print(name[0:4])
+print(name[-7:-3])
