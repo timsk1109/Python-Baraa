7bbc547f6d559b1c6ffa2338aa02a06b02dbf300
Timothy <timothymang09@gmail.com>
2026-09-07 03:45:42 -0500

    strip and upper methods

---
+++ data_clean.py
@@ -0,0 +1,25 @@
+#to clean data of empty spaces we use the "lstrip, rstrip and strip" to do that.
+'''text = " Engineering".lstrip()
+book = "note ".rstrip()
+chapter = "   hello   ".strip()
+pages = "###hello###".strip("#")
+
+content = "  this is nice "
+
+print(text)
+print(book)
+print(chapter)
+print(pages)
+print(len(content.strip()))
+'''
+
+#use of upper and lower, to make sure all word are the same before searching.
+
+data = "python PROGRAMMING"
+print(data.upper())
+print(data.lower())
+
+search = " email ".lower().strip()
+name = "EmaiL ".lower().strip()
+
+print( search == name)
