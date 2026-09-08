86694ef6fabb117abbf72dce5b457dc7b97cf51e
Timothy <timothymang09@gmail.com>
2026-09-08 14:26:12 -0500

    import random and math

--- Numbers/rounding.py
+++ Numbers/rounding.py
@@ -1,6 +1,6 @@
 #rounding numbers and removing +ve or -ve signs
 
-print(abs(2 - 10)) #abs removes the -ve result.
+'''print(abs(2 - 10)) #abs removes the -ve result.
 
 #round(),floor() , ceil() are used to round numbers up or down. you have to import the floor and ceil
 
@@ -10,3 +10,18 @@ print(math.floor(price))
 print(math.ceil(price))
 print(round(price))
 print(round((price, 2)))
+print(math.trunc(price))
+
+
+#random
+
+import random
+
+#print(random.random()) #prints float numbers
+
+print(random.randint(1, 10)) #print integers.'''
+import random
+
+x = random.randint(1, 100)
+print(x)
+print(x % 2)
