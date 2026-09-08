4eebbfeb7481658729a5fe119f6da62ece600cf7
Timothy <timothymang09@gmail.com>
2026-09-08 12:59:15 -0500

    types of numerica data

---
+++ Numbers/types.py
@@ -0,0 +1,14 @@
+#there are types of numbers in python.
+
+x = 5 #integer
+a = 1.02 #float
+b = 2 + 3j #complex
+c = "23"
+
+print(type(b))
+print(type(x))
+print(type(a))
+print(type(c))
+print(type(int(c)))
+
+print(int(c) * 3)
