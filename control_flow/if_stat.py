ab9d5606d92968aba53b0ac63e491068ebd9ed28
Timothy <timothymang09@gmail.com>
2026-09-21 12:07:47 -0500

    nested if

--- control_flow/if_stat.py
+++ control_flow/if_stat.py
@@ -1,12 +1,13 @@
 # if statement used with else.
-score = 30
+score = 99
+project = True
 
 if score >= 90:
     print("A")
 else:
     print("Try again")
 
-#elif statement used 
+#elif statement is  used when there is a follow-up question if the result is false.
 
 if score >= 95:
     print("Your score: " + str(score) +" and grade is A")
@@ -18,3 +19,11 @@ elif score >= 65 and score <= 74:
     print("your score: " + str(score) + " and grade is D")
 else:
     print("You scored below 65, your grade is F")
+    
+    
+# nested If statement is used when there is a follow-up question if there initial if is true!
+if score >= 99:
+    if project == False:
+            print("excellent A+")
+    else:
+        print("Submit your project!!")
